25e482f4bd445b6bc2e8f93513277107bf8808af
Kev1venteur <kvin.gillet@gmail.com>
2019-10-10 13:21:49 +0200

    First commit

---
+++ index.js
@@ -0,0 +1,35 @@
+'use strict';
+
+//On require la classe Demineur
+import Demineur, {Mine, Num} from './Demineur.js';
+
+const demineur = new Demineur();
+const app = new Vue({
+  el: '#demineur',
+  data: {
+    demineur : demineur,
+    tyoto : 'sdf',
+    gamePlayable : true,
+    winOrLooseMessage : '',
+  },
+  methods: {
+    cliquerCase : function (x, y) {
+      if (this.gamePlayable) {
+        demineur.click(x, y)
+        if(demineur.isGameOver && demineur.isWinned){
+          this.gamePlayable = false
+          this.winOrLooseMessage = 'Gagné !!'
+        }
+        else if(demineur.isGameOver && !demineur.isWinned){
+          this.gamePlayable = false
+          this.winOrLooseMessage = 'Perdu !!'
+        }
+      }
+    },
+    flaggerCase : function (x, y) {
+      if (this.gamePlayable) {
+        demineur.flag(x, y)
+      }
+    }
+  }
+});
